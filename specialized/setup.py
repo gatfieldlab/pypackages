9e250621eccd34d85cb479a5de2a4f86f6feaf16
Bulak Arpat <bulak.arpat@gmail.com>
2018-05-28 21:53:48 +0200

    added setup.py

---
+++ specialized/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='specialized',
+    version='0.1.0',
+    description='Collection of specialized data structures and '
+                'utility functions for memory intensive objects',
+#   url='NA',
+    author='Bulak Arpat',
+    author_email='bulak.arpat@gmail.com',
+    license='GPLv3',
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe=False
+)
